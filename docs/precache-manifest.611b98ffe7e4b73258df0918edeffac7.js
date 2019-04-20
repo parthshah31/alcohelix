6376125f872ac0448dfe7c63ca8cc27432e4efa5
Abhinav Venigalla <abhinav.venigalla@gmail.com>
2019-04-20 16:53:22 -0400

    fix relative path in react build

---
+++ docs/precache-manifest.611b98ffe7e4b73258df0918edeffac7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0ddbe69ecf367ae88589f95b5fed469",
+    "url": "./static/media/titleinv.a0ddbe69.png"
+  },
+  {
+    "revision": "e34d2bca4a1754170b9aad44c71ffed8",
+    "url": "./static/media/drink.e34d2bca.svg"
+  },
+  {
+    "revision": "62b80d7701c91e129b74189f3f7309a9",
+    "url": "./static/media/clock.62b80d77.svg"
+  },
+  {
+    "revision": "52a3cfafce91a18c938a5f532790567a",
+    "url": "./static/media/arrows.52a3cfaf.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "ac1aabb9d329d2f830fc",
+    "url": "./static/js/main.ac1aabb9.chunk.js"
+  },
+  {
+    "revision": "74761ff27e743ea81b3a",
+    "url": "./static/js/1.74761ff2.chunk.js"
+  },
+  {
+    "revision": "ac1aabb9d329d2f830fc",
+    "url": "./static/css/main.c9c3d542.chunk.css"
+  },
+  {
+    "revision": "7ed6b9a9ce779a4b0414c4bbb67242c0",
+    "url": "./index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0bb74f037db7ed77dc1c65c50ce4a6db.js"
+  "./precache-manifest.611b98ffe7e4b73258df0918edeffac7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
